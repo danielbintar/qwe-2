1365df3e18e0d8d71a222bc148d5deac44ec3ef2
@danielbintar <daniel_bintar@yahoo.com>
2019-02-03 22:19:42 +0700

    login ui

--- src/game.rs
+++ src/game.rs
@@ -1,6 +1,6 @@
 use amethyst::prelude::*;
 use amethyst::assets::{Loader};
-use amethyst::ui::{UiTransform, Anchor, UiText, TtfFormat};
+use amethyst::ui::{UiTransform, Anchor, UiText, TtfFormat, TextEditing, UiButtonBuilder};
 
 pub struct Game;
 
@@ -8,7 +8,7 @@ impl SimpleState for Game {
     fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
         let world = data.world;
 
-        initialize_ui(world)
+        initialize_ui(world);
     }
 }
 
@@ -23,7 +23,7 @@ fn initialize_ui(world: &mut World) {
 
     let transform = UiTransform::new(
         "title".to_string(), Anchor::TopMiddle,
-        -50., -50., 1., 200., 50., 0,
+        -50., -100., 1., 200., 50., 0,
     );
 
     world
@@ -33,6 +33,88 @@ fn initialize_ui(world: &mut World) {
             font.clone(),
             "Immortal".to_string(),
             [1., 1., 1., 1.],
-            50.,
-        )).build();
+            50.))
+        .build();
+
+
+    let button_builder = UiButtonBuilder::new("login_button", "LOGIN")
+        .with_anchor(Anchor::TopMiddle)
+        .with_position(-50., -500.);
+    button_builder.build_from_world(world);
+
+
+    let transform = UiTransform::new(
+        "username_label".to_string(), Anchor::TopMiddle,
+        -250., -250., 1., 250., 50., 0
+    );
+
+    world
+        .create_entity()
+        .with(transform)
+        .with(UiText::new(
+            font.clone(),
+            "USERNAME:".to_string(),
+            [1., 1., 1., 1.],
+            50.))
+        .build();
+
+
+    let transform = UiTransform::new(
+        "username".to_string(), Anchor::TopMiddle,
+        50., -250., 1., 400., 50., 0
+    );
+
+    world
+        .create_entity()
+        .with(transform)
+        .with(UiText::new(
+            font.clone(),
+            "".to_string(),
+            [1., 1., 1., 1.],
+            50.))
+        .with(TextEditing::new(
+            10,
+            [1., 1., 1., 1.],
+            [0.0, 0.0, 0.0, 1.0],
+            false))
+        .build();
+
+
+    let transform = UiTransform::new(
+        "password_label".to_string(), Anchor::TopMiddle,
+        -250., -350., 1., 250., 50., 0
+    );
+
+    world
+        .create_entity()
+        .with(transform)
+        .with(UiText::new(
+            font.clone(),
+            "PASSWORD:".to_string(),
+            [1., 1., 1., 1.],
+            50.))
+        .build();
+
+
+    let transform = UiTransform::new(
+        "password".to_string(), Anchor::TopMiddle,
+        50., -350., 1., 400., 50., 0
+    );
+
+    let password = world
+        .create_entity()
+        .with(transform)
+        .with(UiText::new(
+            font.clone(),
+            "".to_string(),
+            [1., 1., 1., 1.],
+            50.))
+        .with(TextEditing::new(
+            10,
+            [1., 1., 1., 1.],
+            [0.0, 0.0, 0.0, 1.0],
+            false))
+        .build();
+    let mut ui_text_storage = world.write_storage::<UiText>();
+    ui_text_storage.get_mut(password).unwrap().password = true;
 }
