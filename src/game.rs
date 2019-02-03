6b52d1f6ece6ac1b607fff1434ad5fe0f5b4d82b
@danielbintar <daniel_bintar@yahoo.com>
2019-02-03 17:11:24 +0700

    amethyst

---
+++ src/game.rs
@@ -0,0 +1,38 @@
+use amethyst::prelude::*;
+use amethyst::assets::{Loader};
+use amethyst::ui::{UiTransform, Anchor, UiText, TtfFormat};
+
+pub struct Game;
+
+impl SimpleState for Game {
+    fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        let world = data.world;
+
+        initialize_ui(world)
+    }
+}
+
+fn initialize_ui(world: &mut World) {
+    let font = world.read_resource::<Loader>().load(
+        "./resources/font/square.ttf",
+        TtfFormat,
+        Default::default(),
+        (),
+        &world.read_resource(),
+    );
+
+    let transform = UiTransform::new(
+        "title".to_string(), Anchor::TopMiddle,
+        -50., -50., 1., 200., 50., 0,
+    );
+
+    world
+        .create_entity()
+        .with(transform)
+        .with(UiText::new(
+            font.clone(),
+            "Immortal".to_string(),
+            [1., 1., 1., 1.],
+            50.,
+        )).build();
+}
