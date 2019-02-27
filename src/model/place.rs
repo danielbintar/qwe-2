96a491af0c7f3fe5c1b8c3d65735809207a86454
danielbintar <daniel.bintar@bukalapak.com>
2019-02-27 09:55:49 +0700

    move by place (#28)

---
+++ src/model/place.rs
@@ -0,0 +1,20 @@
+use serde_derive::Deserialize;
+
+#[derive(Deserialize, PartialEq, Clone)]
+#[serde(rename_all = "snake_case")]
+pub enum Place {
+    Region,
+    Town
+}
+
+pub struct CurrentPlace {
+    pub place: Option<Place>,
+}
+
+impl Default for CurrentPlace {
+    fn default() -> Self {
+        Self {
+            place: None
+        }
+    }
+}
